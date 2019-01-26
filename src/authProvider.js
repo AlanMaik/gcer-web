ee6cb92edcf14e8648426b569f09a4d55196cb5b
Alan Maik <alanmaikm@gmail.com>
2019-01-26 17:55:52 -0300

    heroku link

--- src/authProvider.js
+++ src/authProvider.js
@@ -4,7 +4,7 @@ export default (type, params) => {
     if (type === AUTH_LOGIN) {
         const { username, password } = params;
         var email = username;
-        const request = new Request('http://localhost:5000/auth/sign_in', {
+        const request = new Request('https://gcer-api.herokuapp.com/auth/sign_in', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: new Headers({ 'Content-Type': 'application/json' }),
