055c697f88b8e0e5aa3ddc743d3fc60ac36d4abe
Alan Maik <alanmaikm@gmail.com>
2019-01-28 23:08:53 -0300

    circle ci

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
-import dataProvider from './dataProvider';
 import NotFound from './NotFound';
 import { UserList } from './users';
 
-// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin catchAll={NotFound} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+  <Admin catchAll={NotFound} dashboard={Dashboard} authProvider={authProvider} >
       <Resource name="users" list={UserList} />
   </Admin>
 );
