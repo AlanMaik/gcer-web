d3076f262f596ff2fb62693a4651594adbfb0b74
Alan Maik <alanmaikm@gmail.com>
2019-02-02 01:37:34 -0300

    Fix lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Admin, Resource } from 'react-admin'
 import './App.css'
+import jsonServerProvider from 'ra-data-json-server'
 import Dashboard from './Dashboard'
 import authProvider from './authProvider'
 import NotFound from './NotFound'
 import { UserList } from './users'
 import Login from './Login'
-import jsonServerProvider from 'ra-data-json-server';
-const dataProvider = jsonServerProvider(process.env.REACT_APP_API_URL);
+const dataProvider = jsonServerProvider(process.env.REACT_APP_API_URL)
 
 const App = () => (
   <Admin
