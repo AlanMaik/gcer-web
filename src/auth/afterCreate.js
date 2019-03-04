c99f073eaf97954b8b2856fbad443beb88c56b41
Alan Maik <alanmaikm@gmail.com>
2019-03-04 09:09:53 -0300

    Delete files from the auth folder

--- src/auth/afterCreate.js
+++
@@ -1,8 +0,0 @@
-export default (
-  { screenProps, navigation },
-  route = 'ConfirmationCode',
-) => async ({ data }) => {
-  const { email } = data
-  await screenProps.setCurrentUser(data)
-  navigation.navigate(route, { email })
-}
--- src/auth/logout.js
+++
@@ -1,8 +0,0 @@
-import { removeAsyncStorage } from '@/asyncStorage'
-import credentialsKey from 'auth/credentialsKey'
-
-export default ({ setCurrentUser }, { navigate }) => async () => {
-  await removeAsyncStorage(credentialsKey)
-  navigate('Auth')
-  setCurrentUser(null)
-}
