df4c20385e75c7b5f8f7031f8b9d549f153f6bf7
maurolsn <mauroleandrosn@gmail.com>
2019-03-05 20:58:00 -0300

    Shedule-Data-Time

---
+++ src/shedule/DataPicker.js
@@ -0,0 +1,79 @@
+import "date-fns";
+import React from "react";
+import format from "date-fns/format";
+import PropTypes from "prop-types";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  InlineDateTimePicker
+} from "material-ui-pickers";
+
+import ptLocale from "date-fns/locale/pt-BR";
+
+class LocalizedUtils extends DateFnsUtils {
+    getDatePickerHeaderText(date) {
+      return format(date, { locale: this.locale });
+    }
+  }
+
+
+
+class DataPicker extends React.Component {
+  state = {
+    // The first commit of Material-UI
+    selectedDate: new Date(),
+  };
+  
+
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { selectedDate, locale } = this.state;
+
+    return (
+      <MuiPickersUtilsProvider utils={(DateFnsUtils , LocalizedUtils)} locale={ptLocale}>
+        <Grid >
+          <div className="picker">
+            <InlineDateTimePicker
+              keyboard
+              ampm={false}
+              value={selectedDate}
+              onChange={this.handleDateChange}
+              onError={console.log}
+              disablePast
+              format={"dd/MM/yyyy HH:mm"}
+              mask={[
+                /\d/,
+                /\d/,
+                "/",
+                /\d/,
+                /\d/,
+                "/",
+                /\d/,
+                /\d/,
+                /\d/,
+                /\d/,
+                " ",
+                /\d/,
+                /\d/,
+                ":",
+                /\d/,
+                /\d/
+              ]}
+            />
+          </div>
+        </Grid>
+      </MuiPickersUtilsProvider>
+    );
+  }
+}
+
+DataPicker.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default (DataPicker);
--- src/shedule/SheduleCreate.js
+++ src/shedule/SheduleCreate.js
@@ -3,12 +3,13 @@ import {
   TextInput,
   Create,
   SelectInput,
-  SimpleForm,
-  DateInput,
-  NumberInput,
-  RadioButtonGroupInput,
+  SimpleForm
 } from 'react-admin'
+
 import { withStyles } from '@material-ui/core/styles'
+import DatePicker from 'react-datepicker'
+import DataPicker from './DataPicker'
+
 
 export const styles = {
   inlineBlock: { display: 'inline-flex', marginRight: '1rem' },
@@ -16,44 +17,45 @@ export const styles = {
 
 const SheduleCreate = ({ classes, ...props }) => (
   <Create title="Agendamento" {...props}>
-    <SimpleForm>      
-        <TextInput
-          source="name"
-          label="Nome"
-          formClassName={classes.inlineBlock}
-        />
-        <TextInput
-          source="last_name"
-          label="Sobrenome"
-          formClassName={classes.inlineBlock}
-        />
-        <TextInput
-          source="phone"
-          label="Telefone"
-          type="phone"
-          formClassName={classes.inlineBlock}
-        />
-        <DateInput
-          source="date_shedule"
-          label="Data"          
-          formClassName={classes.inlineBlock}
-        />
-        <SelectInput
-          label="Serviço"
-          source="service"
-          formClassName={classes.inlineBlock}
-          choices={[
-            { id: 'pollo', name: 'Poldologa' },
-            { id: 'massage', name: 'massage' },
-            { id: 'hair_removal', name: 'Depilação' },
-          ]}
-        />
-        <TextInput
-          source="profession"
-          label="Profissional"
-          formClassName={classes.inlineBlock}
-        />
-        
+    <SimpleForm>
+      <TextInput
+        source="name"
+        label="Nome"
+        formClassName={classes.inlineBlock}
+      />
+      <TextInput
+        source="last_name"
+        label="Sobrenome"
+        formClassName={classes.inlineBlock}
+      />
+      <TextInput
+        source="phone"
+        label="Telefone"
+        type="phone"
+        formClassName={classes.inlineBlock}
+      />
+      <DataPicker
+        source="date_shedule"
+        label="Data"
+        formClassName={classes.inlineBlock}
+      />      
+      <SelectInput
+        label="Serviço"
+        source="service"
+        formClassName={classes.inlineBlock}
+        choices={[
+          { id: 'pollo', name: 'Poldologa' },
+          { id: 'massage', name: 'massage' },
+          { id: 'hair_removal', name: 'Depilação' },
+        ]}
+      />
+
+      <TextInput
+        source="profession"
+        label="Profissional"
+        formClassName={classes.inlineBlock}
+      />
+
     </SimpleForm>
   </Create>
 )
